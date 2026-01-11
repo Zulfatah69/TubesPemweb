b06b061304fddb2be108ed53459d61b62e8e5da1
Zulfatah69 <zulfatah.izadin@mhs.itenas.ac.id>
2026-01-12 00:46:17 +0700

    tes cld5

--- config/app.php
+++ config/app.php
@@ -76,7 +76,8 @@ return [
         * Application Service Providers...
         */
         App\Providers\AppServiceProvider::class,
-
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
     ],
 
 
