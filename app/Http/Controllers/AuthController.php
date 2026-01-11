a7ea7edf203e4dda930f169d209d3068129b35b3
Zulfatah69 <zulfatah.izadin@mhs.itenas.ac.id>
2026-01-11 18:02:28 +0700

    ahhhhhhhhhh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;   // ⬅️ TAMBAHKAN INI
 use App\Services\FirebaseService;
 use App\Models\User;
 
