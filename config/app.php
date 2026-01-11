ebac56c04983c19b231068ac4ab82b981d67371a
Zulfatah69 <zulfatah.izadin@mhs.itenas.ac.id>
2026-01-12 00:37:04 +0700

    tes cld4

--- config/app.php
+++ config/app.php
@@ -41,6 +41,9 @@ return [
 
     'providers' => [
 
+        /*
+        * Laravel Framework Service Providers...
+        */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -64,14 +67,18 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        // Cloudinary
+        /*
+        * Package Service Providers...
+        */
         CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
 
-        // App providers
+        /*
+        * Application Service Providers...
+        */
         App\Providers\AppServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+
     ],
+
 
 
     /*
