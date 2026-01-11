cdca795c9f72d88f9c2568ceae89fbda6e82e053
Zulfatah69 <zulfatah.izadin@mhs.itenas.ac.id>
2026-01-11 21:05:53 +0700

    crach

--- database/migrations/2026_01_11_135342_add_email_verified_at_to_users_table.php
+++ database/migrations/2026_01_11_135342_add_email_verified_at_to_users_table.php
@@ -12,14 +12,18 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('email_verified_at')->nullable()->after('email');
+            if (!Schema::hasColumn('users', 'email_verified_at')) {
+                $table->timestamp('email_verified_at')->nullable()->after('email');
+            }
         });
     }
 
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('email_verified_at');
+            if (Schema::hasColumn('users', 'email_verified_at')) {
+                $table->dropColumn('email_verified_at');
+            }
         });
     }
 
